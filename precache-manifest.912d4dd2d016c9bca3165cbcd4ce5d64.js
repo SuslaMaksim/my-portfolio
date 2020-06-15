ea9073fd17df1f5799f25e815a1e6371b5251bce
SuslaMaksim <susla.maksim@gmail.com>
2020-06-15 12:38:23 +0300

    Updates

---
+++ precache-manifest.912d4dd2d016c9bca3165cbcd4ce5d64.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9367160feb7554eacc34ed84cf855064",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "657e46ca1dd813accc7e",
+    "url": "/my-portfolio/static/css/main.8f1eb113.chunk.css"
+  },
+  {
+    "revision": "56e97cf177d1af606794",
+    "url": "/my-portfolio/static/js/2.7f756c38.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/my-portfolio/static/js/2.7f756c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657e46ca1dd813accc7e",
+    "url": "/my-portfolio/static/js/main.ff0bcfa6.chunk.js"
+  },
+  {
+    "revision": "feb14810df33d645b05c",
+    "url": "/my-portfolio/static/js/runtime-main.9efc66b9.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/my-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "06795066e8e2e01186d7087289ad6100",
+    "url": "/my-portfolio/static/media/poceDesk.06795066.jpg"
+  },
+  {
+    "revision": "e16dca8781165b546546e136fccadcc8",
+    "url": "/my-portfolio/static/media/portfolioPicture.e16dca87.jpg"
+  },
+  {
+    "revision": "7f5261d5dae11c657a8b0e563ef3be1e",
+    "url": "/my-portfolio/static/media/sohialNetvork.7f5261d5.jpg"
+  },
+  {
+    "revision": "22cc33e48d17ab159d1ae8915a2c8fa9",
+    "url": "/my-portfolio/static/media/susla.avatar.22cc33e4.jpg"
+  }
+]);
